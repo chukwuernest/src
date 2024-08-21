906d0b423a1cc8d958610fed1759a3164735021c
chukwuernest <chukwu17mc@gmail.com>
2024-08-21 10:54:32 -0400

    update

--- App.jsx
+++ App.jsx
@@ -9,6 +9,42 @@ function App() {
   let [todoList, setTodoList] = useState([])
   let [isLoading, setIsLoading] = useState(true)
 
+  async function fetchData() {
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`,
+      },
+    }
+    const url = `https://api.airtable.com/v0/${
+      import.meta.env.VITE_AIRTABLE_BASE_ID
+    }/${import.meta.env.VITE_TABLE_NAME}`
+
+    try {
+      const response = await fetch(url, options)
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`)
+      }
+
+      const data = await response.json()
+
+      const todos = data.records.map((record) => ({
+        id: record.id,
+        title: record.fields.title,
+      }))
+
+      setTodoList(todos)
+      setIsLoading(false)
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   useEffect(() => {
     const loadTodoList = () => {
       return new Promise((resolve) => {
