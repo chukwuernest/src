8d6a42d9099d1bf64a2310478a47464392a621ab
chukwuernest <chukwu17mc@gmail.com>
2024-08-28 09:44:03 -0400

    update

--- App.jsx
+++ App.jsx
@@ -5,7 +5,6 @@ import TodoList from './TodoList'
 import AddTodoForm from '../AddTodoForm'
 
 function App() {
-  //  const useSemiPersistentState = () => {
   let [todoList, setTodoList] = useState([])
   let [isLoading, setIsLoading] = useState(true)
 
@@ -68,39 +67,16 @@ function App() {
     }
   }, [todoList, isLoading])
 
-  // useEffect(() => {
-  //   localStorage.setItem('savedTodoList', JSON.stringify(todoList))
-  //   console.log(todoList)
-  // }, [todoList])
-  // let [newTodo, setNewTodo] = useState('')
   function addTodo(newTodo) {
     setTodoList([...todoList, newTodo])
     console.log(newTodo)
   }
-  // const useSemiPersistentState = () => {
-  //   const savedTodoList = localStorage.getItem('savedTodoList')
-  //   return savedTodoList ? JSON.parse(savedTodoList) : []
-  // }
-
-  // let [todoList, setTodoList] = useState(useSemiPersistentState())
-
-  // useEffect(() => {
-  //   localStorage.setItem('savedTodoList', JSON.stringify(todoList))
-  //   console.log(todoList)
-  // }, [todoList])
 
   const removeTodo = (id) => {
     const updatedTodoList = todoList.filter((item) => item.id !== id)
     setTodoList(updatedTodoList)
   }
-  // return (
-  //   <>
-  //     <h1>Todo List</h1>
-  //     <AddTodoForm onAddTodo={addTodo} />
-  //     <p>{}</p>
-  //     <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
-  //   </>
-  // )
+
   return (
     <BrowserRouter>
       <Routes>
